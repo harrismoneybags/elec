646a905e47386407994758d95e16a40724ae9077
Heathen <heathenacf97@icloud.com>
2022-03-11 18:10:35 +0000

    inital comit

---
+++ run.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template
+
+#make an instance 
+app = Flask(__name__)
+
+# make a decorater
+@app.route('/')
+
+#def index():
+	#return "<h1> Hello world </h1>"
+
+
+@app.route('/')
+
+def index():
+	return render_template("index.html")
+
+
+@app.route('/services')
+
+def services():
+	return render_template("services.html")
+
+
+@app.route('/about')
+
+def about():
+	return render_template("about.html")
+
+#costem error pages
+
+#invalied url
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template("404.html"),404
+
+#internal server error
+@app.errorhandler(500)
+def page_not_found(e):
+	return render_template("500.html"),500
